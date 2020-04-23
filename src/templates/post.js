f6cff1dfd175f1b3d0d45d03ca0bc612a5ada012
Majal <4157956+majal@users.noreply.github.com>
2020-04-23 05:42:33 +0000

    worked on sed "s/Work/Post/g"

---
+++ src/templates/post.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Slider from 'react-slick'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+  <Layout>
+    <article className="sheet">
+      <HelmetDatoCms seo={data.datoCmsPost.seoMetaTags} />
+      <div className="sheet__inner">
+        <h1 className="sheet__title">{data.datoCmsPost.title}</h1>
+        <p className="sheet__lead">{data.datoCmsPost.excerpt}</p>
+        <div className="sheet__slider">
+          <Slider infinite={true} slidesToShow={2} arrows>
+            {data.datoCmsPost.gallery.map(({ fluid }) => (
+              <img alt={data.datoCmsPost.title} key={fluid.src} src={fluid.src} />
+            ))}
+          </Slider>
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: data.datoCmsPost.descriptionNode.childMarkdownRemark.html,
+          }}
+        />
+        <div className="sheet__gallery">
+          <Img fluid={data.datoCmsPost.coverImage.fluid} />
+        </div>
+      </div>
+    </article>
+  </Layout>
+)
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    datoCmsPost(slug: { eq: $slug }) {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      title
+      excerpt
+      gallery {
+        fluid(maxWidth: 200, imgixParams: { fm: "jpg", auto: "compress" }) {
+          src
+        }
+      }
+      descriptionNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+      coverImage {
+        url
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
+    }
+  }
+`
